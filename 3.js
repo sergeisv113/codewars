0e954d171c87656642cd37f1297ea3a2485bb042
Sergei <sergeisv113@gmail.com>
2022-02-04 17:09:01 +0200

    '13'

--- 3.js
+++ 3.js
@@ -1,6 +1,16 @@
-function boolToWord( bool ){
-if (bool) {
-    return "Yes";
+function howMuchILoveYou(nbPetals) {
+   if (nbPetals == 0) {
+       return 'I love you';
+   } else  if (nbPetals == 1) {
+       return 'a little';
+   } else  if (nbPetals == 2) {
+       return 'a lot';
+   } else  if (nbPetals == 3) {
+       return 'passionately';
+   } else if (nbPetals == 4) {
+       return  'madly';
+   } else {
+       return 'not at all';
+   }
 }
-return "No";
-};
+howMuchILoveYou(7)
--- script.js
+++ script.js
@@ -146,3 +146,5 @@ function boolToWord( bool ){
     }
     return "No";
 };
+//любит не любит
+howMuchILoveYou = n => ['I love you', 'a little', 'a lot', 'passionately', 'madly', 'not at all'][(n - 1) % 6]
