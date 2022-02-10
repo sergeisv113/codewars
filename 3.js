07278da3fffde155821d3e52ea34896f2b7901d2
Sergei <sergeisv113@gmail.com>
2022-02-10 12:37:33 +0200

    '13'

--- 3.js
+++ 3.js
@@ -1,5 +1,4 @@
-function arrayPlusArray(arr1, arr2) {
-   let sum1 = arr1.reduce((sum, el) => sum + el, 0);
-   let sum2 = arr2.reduce((sum, el) => sum + el, 0);
-    return sum1 + sum2;
+function stringToArray(string){
+    return  string.split(' ');
 }
+stringToArray("Robin Singh")
--- script.js
+++ script.js
@@ -287,3 +287,8 @@ function arrayPlusArray(arr1, arr2) {
     let sum2 = arr2.reduce((sum, el) => sum + el, 0);
     return sum1 + sum2;
 }
+//которая разбивает строку и преобразует ее в массив слов.
+function stringToArray(string){
+    return  string.split(' ');
+}
+stringToArray("Robin Singh")
